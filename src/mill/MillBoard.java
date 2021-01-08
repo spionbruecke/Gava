c2686823f14e05d11cea519b3fcadb2f0c2be9c9
spionbruecke <alexander.posch@live.de>
2021-01-08 18:57:20 +0100

    Board To String converter

--- src/mill/MillBoard.java
+++ src/mill/MillBoard.java
@@ -9,6 +9,9 @@ public class MillBoard extends GameBoard {
     private int counterBlack = 9;
     private int counterWhite = 9;
 
+    public MillBoard(){
+        super.setState(setUpBoard());
+    }
 
     @Override
     public PlayingPiece[] getPlayingPieces() {
@@ -27,24 +30,26 @@ public class MillBoard extends GameBoard {
         PlayingPiece[][] initialState = new PlayingPiece[7][];
 
         for(int k = 0 ; k < 7; k ++) {
-            if(k < 4 || k >4)
+            if(k < 3 || k >3)
                 initialState[k] = new PlayingPiece[3];
             else
                 initialState[k] = new PlayingPiece[6];
         }
 
         for(int k = 0 ; k < 7; k ++) {
-            if(k == 4)
+            if(k == 3)
                 for (int j = 0; j < 6; j ++){
                     initialState[k][j] = new PlayingPiece();
                     initialState[k][j].setName("null");
                     initialState[k][j].setColour("null");
+                    initialState[k][j].setPosition(MillMoveConverter.convertArrayCoordinateIntoPosRow(k)+ MillMoveConverter.convertArrayCoordinateIntoPosColumn(j));
                 }
             else
                 for (int j = 0; j < 3; j ++){
                     initialState[k][j] = new PlayingPiece();
                     initialState[k][j].setName("null");
                     initialState[k][j].setColour("null");
+                    initialState[k][j].setPosition(MillMoveConverter.convertArrayCoordinateIntoPosRow(k)+ MillMoveConverter.convertArrayCoordinateIntoPosColumn(j));
                 }
         }
 
--- src/mill/MillMoveConverter.java
+++ src/mill/MillMoveConverter.java
@@ -16,10 +16,110 @@ public class MillMoveConverter implements MoveConverter {
         return null;
     }
 
+    public static void main(String[] args) {
+        MillBoard mill = new MillBoard();
+        System.out.println(convertPiecesToString(mill));
+    }
+
     //TODO(Alex) Umwandlung des Spielbretts in einen String 
     public static String convertPiecesToString(MillBoard board) {
+        StringBuilder output = new StringBuilder();
+        PlayingPiece[][] state = board.getState();
 
+
+        for(int k = 0 ; k < 7; k ++) {
+            if(k == 3)
+                for (int j = 0; j < 6; j ++){
+                    output.append("<").append(state[k][j].toString()).append(">");
+                }
+            else
+                for (int j = 0; j < 3; j ++){
+                    output.append("<").append(state[k][j].toString()).append(">");
+                }
+        }
+
+
+        return output.toString() ;
+    }
+
+     /**
+     * Converts an integer array coordinate into a String description.
+     * @param i int
+     * @return String
+     */
+    static String convertArrayCoordinateIntoPosRow(int i){
+        switch (i){
+            case 0:
+                return "7";
+
+            case 1:
+                return "6";
+
+            case 2:
+                return "5";
+
+            case 3:
+                return "4";
+
+            case 4:
+                return "3";
+
+            case 5:
+                return "2";
+
+            case 6:
+                return "1";
+
+            default:
+                System.out.println("Error convert into PosRow: " + i);
+                return "";
+        }
+    }
+
+    /**
+     * Converts an integer array coordinate into a String description.
+     * @param i int
+     * @return String
+     */
+    static String convertArrayCoordinateIntoPosColumn(int i){
+        switch (i){
+            case 0:
+                return "A";
+
+            case 1:
+                return "B";
+
+            case 2:
+                return "C";
+
+            case 3:
+                return "D";
+
+            case 4:
+                return "E";
+
+            case 5:
+                return "F";
+
+            case 6:
+                return "G";
+
+            default:
+                return "";
+        }
+    }
+
+     /**
+     * Converts the String list into a two dim. PlayingPiece Array
+     * @param input String
+     * @return PlayingPiece[][]
+     * @throws WrongFormatException
+     */
+    public static PlayingPiece[][] getBoardFromString(String input) throws WrongFormatException {
+       
+  
         return null;
     }
+
     
 }
