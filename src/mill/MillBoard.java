648f599e82d6499b42cd0971530341d2327fdae1
spionbruecke <alexander.posch@live.de>
2021-01-09 10:50:14 +0100

    String to Board and Board to String

--- src/mill/MillBoard.java
+++ src/mill/MillBoard.java
@@ -8,21 +8,26 @@ public class MillBoard extends GameBoard {
 
     private int counterBlack = 9;
     private int counterWhite = 9;
+    private PlayingPiece[] playingPieces = new PlayingPiece[18];
 
     public MillBoard(){
         super.setState(setUpBoard());
+
+        for(int i = 0; i < 18; i ++){
+            playingPieces[i] = new PlayingPiece();
+            playingPieces[i].setName("token");
+            if(i < 9)
+                playingPieces[i].setColour("white");
+            else
+                playingPieces[i].setColour("black");
+            playingPieces[i].setPosition("null");
+        }
     }
 
     @Override
     public PlayingPiece[] getPlayingPieces() {
         // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setNewBoard(String input) throws WrongFormatException {
-        // TODO Auto-generated method stub
-
+        return playingPieces;
     }
 
     @Override
@@ -42,18 +47,55 @@ public class MillBoard extends GameBoard {
                     initialState[k][j] = new PlayingPiece();
                     initialState[k][j].setName("null");
                     initialState[k][j].setColour("null");
-                    initialState[k][j].setPosition(MillMoveConverter.convertArrayCoordinateIntoPosRow(k)+ MillMoveConverter.convertArrayCoordinateIntoPosColumn(j));
                 }
             else
                 for (int j = 0; j < 3; j ++){
                     initialState[k][j] = new PlayingPiece();
                     initialState[k][j].setName("null");
                     initialState[k][j].setColour("null");
-                    initialState[k][j].setPosition(MillMoveConverter.convertArrayCoordinateIntoPosRow(k)+ MillMoveConverter.convertArrayCoordinateIntoPosColumn(j));
                 }
         }
 
         return initialState;
     }
     
+    @Override
+    public void setNewBoard(String input) throws WrongFormatException {
+        MillBoard board = new MillBoard();
+        PlayingPiece[][] newBoard = board.getState();
+        StringBuilder position = new StringBuilder();
+        int counter = 0;
+        int column;
+        int row;
+
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == '='){
+
+                if(input.charAt(i+1) != 'n')
+                    position.append(input.charAt(i + 1)).append(input.charAt(i + 2));
+                else    
+                    position.append("null");
+
+                if(!this.getPlayingPieces()[counter].getPosition().equals(position.toString()))
+                    this.getPlayingPieces()[counter].setHasMoved();
+
+                this.getPlayingPieces()[counter].setPosition(position.toString());
+                
+                if(input.charAt(i - 1) == '1')
+                    this.getPlayingPieces()[counter].setHasMoved();
+                    
+                row = MillMoveConverter.convertPosIntoArrayCoordinate(input.charAt(i+2));
+                column = MillMoveConverter.convertPosIntoArrayCoordinate(input.charAt(i + 1));
+            
+                if(row != -2 && column != -2)
+                    newBoard[row][column] = this.getPlayingPieces()[counter];
+
+                position = new StringBuilder();
+                counter++;
+                i = i + 3;
+            }
+        }
+
+        this.setState(newBoard);
+    }
 }
--- src/mill/MillMoveConverter.java
+++ src/mill/MillMoveConverter.java
@@ -18,24 +18,28 @@ public class MillMoveConverter implements MoveConverter {
 
     public static void main(String[] args) {
         MillBoard mill = new MillBoard();
+        MillBoard newMill = new MillBoard();
+
+        try {
+            newMill.setNewBoard("<token,white,0=A7><token,white,0=C4><token,white,0=null><token,white,0=null><token,white,0=null><token,white,0=null><token,white,0=null><token,white,0=null><token,white,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null><token,black,0=null>");
+            mill.setNewBoard(convertPiecesToString(newMill));
+        
+        } catch (WrongFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
         System.out.println(convertPiecesToString(mill));
     }
 
-    //TODO(Alex) Umwandlung des Spielbretts in einen String 
+    //TODO(Alex) Umwandlung der Spielfiguren in einen String 
     public static String convertPiecesToString(MillBoard board) {
         StringBuilder output = new StringBuilder();
-        PlayingPiece[][] state = board.getState();
+        PlayingPiece[] playingPieces = board.getPlayingPieces();
 
 
-        for(int k = 0 ; k < 7; k ++) {
-            if(k == 3)
-                for (int j = 0; j < 6; j ++){
-                    output.append("<").append(state[k][j].toString()).append(">");
-                }
-            else
-                for (int j = 0; j < 3; j ++){
-                    output.append("<").append(state[k][j].toString()).append(">");
-                }
+        for(int i = 0 ; i < 18; i ++) {
+            output.append("<").append(playingPieces[i].toString()).append(">");
         }
 
 
@@ -109,6 +113,49 @@ public class MillMoveConverter implements MoveConverter {
         }
     }
 
+    /**
+     * Converts a String description into an integer array coordinate.
+     * @param i char
+     * @return int
+     */
+    static int convertPosIntoArrayCoordinate(char i){
+        switch (i){
+            case '7':
+            case 'A':
+                return 0;
+
+            case '6':
+            case 'B':
+                return 1;
+
+            case '5':
+            case 'C':
+                return 2;
+
+            case '4':
+            case 'D':
+                return 3;
+
+            case '3':
+            case 'E':
+                return 4;
+
+            case '2':
+            case 'F':
+                return 5;
+
+            case '1':
+                return 6;
+
+            case 'n':
+            case 'u':
+                return -2;
+            default:
+                System.out.println("Error convert PosRow into Coordinate: " + i);
+                return -1;
+        }
+    }
+
      /**
      * Converts the String list into a two dim. PlayingPiece Array
      * @param input String
@@ -116,9 +163,37 @@ public class MillMoveConverter implements MoveConverter {
      * @throws WrongFormatException
      */
     public static PlayingPiece[][] getBoardFromString(String input) throws WrongFormatException {
-       
+        MillBoard board = new MillBoard();
+        PlayingPiece[][] newBoard = board.getState();
+        StringBuilder position = new StringBuilder();
+        int row;
+        int column;
+        int counter = 0;
+
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == '='){
+                if(input.charAt(i + 1) == 'n')
+                    position.append("null");
+                else
+                    position.append(input.charAt(i + 1)).append(input.charAt(i + 2));
+
+                if(input.charAt(i - 1) == '1')
+                    board.getPlayingPieces()[counter].setHasMoved();
+
+                board.getPlayingPieces()[counter].setPosition(position.toString());
+                row = convertPosIntoArrayCoordinate((input.charAt(i+2)));
+                column = convertPosIntoArrayCoordinate(input.charAt(i + 1));
+                if (row != -2 && column != -2)
+                    newBoard[row][column] = board.getPlayingPieces()[counter];
+
+                
+                position = new StringBuilder();
+                counter++;
+                i = i + 3;
+            }
+        }
   
-        return null;
+        return newBoard;
     }
 
     
